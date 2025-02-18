40ecc490bc74bba27e4d27860bb2453bbd55679b
Lucas Trindade <trindadebra@gmail.com>
2025-02-17 21:31:33 -0300

    Biome added

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,31 +1,33 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import { z } from "zod";
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
-export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
-    app.post("/subscriptions", {
-        schema: {
-            summary: "Subscribe to an event",
-            tags: ["Subscriptions"],
-            description: "Subscribe to an event",
-            body: z.object({
-                name: z.string(),
-                email: z.string().email(),
-            }),
-            response: {
-                201: z.object({
-                    name: z.string(),
-                    email: z.string(),
-                }),
-            },
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribe to an event',
+        tags: ['Subscriptions'],
+        description: 'Subscribe to an event',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string(),
+          }),
         },
-    }, async (request, reply) => {
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body
 
-        const { name, email } = request.body;
-
-        return reply.status(201).send({
-            name,
-            email,
-        });
-    });
-};
-
+      return reply.status(201).send({
+        name,
+        email,
+      })
+    }
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,41 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
-import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
-import { env } from "./env";
+import { fastifyCors } from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
-
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors, {
-    origin: true,
-});
+  origin: true,
+})
 
 app.register(fastifySwagger, {
-    openapi: {
-        info: {
-            title: "NLW Connect",
-            version: "1.0.0",
-        }
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '1.0.0',
     },
-    transform: jsonSchemaTransform,
-});
+  },
+  transform: jsonSchemaTransform,
+})
 
 app.register(fastifySwaggerUi, {
-    routePrefix: "/docs",
-});
+  routePrefix: '/docs',
+})
 
-app.register(subscribeToEventRoute);
+app.register(subscribeToEventRoute)
 
 app.listen({ port: env.PORT }).then(() => {
-    console.log("HTTP Server Running!");
-});
+  console.log('HTTP Server Running!')
+})
